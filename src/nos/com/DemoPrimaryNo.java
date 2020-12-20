702e40b6afad9f2df5c5222e2765913d2160fc1b
Ashish <Ashish@Ashish>
2020-12-20 10:50:42 +0530

    local commit primary no.

---
+++ src/nos/com/DemoPrimaryNo.java
@@ -0,0 +1,26 @@
+package nos.com;
+
+public class DemoPrimaryNo {
+	// int[] no=new int[100];
+	// int primaryNoFound
+
+	public static void main(String[] args) {
+		for (int i = 2; i < 100; i++) {
+			boolean flag = true;
+			for (int j = 2; j < i; j++) {
+				if (i % j == 0) {
+					flag = false;
+                     break;
+				}
+				
+
+			}
+               if(flag==true)
+               {
+            	   System.out.println(i);
+            	   
+               }
+			
+		}
+	}
+}
